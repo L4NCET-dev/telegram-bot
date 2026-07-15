db366423e01e32bf1d7711c7651d452214c7009c
L4NCET-dev <vadym.hlotka@gmail.com>
2026-07-15 18:08:18 +0300

    Initial commit

---
+++ src/test/java/org/l4ncet/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.l4ncet.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
