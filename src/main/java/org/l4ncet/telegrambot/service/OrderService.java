01fecd2835485b3e4c45bb2524067c0184490926
L4NCET-dev <vadym.hlotka@gmail.com>
2026-07-30 22:45:45 +0300

    Add  Order Service

--- src/main/java/org/l4ncet/telegrambot/service/OrderService.java
+++ src/main/java/org/l4ncet/telegrambot/service/OrderService.java
@@ -1,7 +1,26 @@
 package org.l4ncet.telegrambot.service;
 
+import lombok.RequiredArgsConstructor;
+import org.l4ncet.telegrambot.dto.CreateOrderRequestDto;
+import org.l4ncet.telegrambot.dto.OrderResponseDto;
+import org.l4ncet.telegrambot.entity.Order;
+import org.l4ncet.telegrambot.mapper.OrderMapper;
+import org.l4ncet.telegrambot.repository.OrderRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final OrderMapper orderMapper;
+
+    @Transactional
+    public OrderResponseDto createOrder(CreateOrderRequestDto requestDto) {
+        Order order = orderMapper.toEntity(requestDto);
+        Order savedOrder = orderRepository.save(order);
+
+        return orderMapper.toResponseDto(savedOrder);
+    }
 }
